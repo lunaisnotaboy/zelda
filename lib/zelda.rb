dc7f37e494004377f21e8c2481ddb77f249e072f
lunaisnotaboy <her@mint.lgbt>
2021-01-12 18:31:38 -0500

    Inital Commit

---
+++ lib/zelda.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "zelda/version"
+
+module Zelda
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/zelda/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module Zelda
+  VERSION = "0.1.0"
+end
